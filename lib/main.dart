1b6009ab3e4cb53d07c5c4dedee62d6ee6ca673f
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-11-26 10:53:31 -0300

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mega Sorte',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
